796d35e6f32b18807c848c224650c11ef7df4741
Lionx07 <ragamafi07@gmail.com>
2024-03-31 18:02:33 -0300

    WIP

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 using SignalRChatServer;
 
+
+// Program file...
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure services
